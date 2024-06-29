39ecc54fde926c9aed19eb8d06ed011750286e4a
Aeneas <tjr9320@gmail.com>
2024-06-29 21:22:43 +0900

    remvoe favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
     return (
       <Html lang={CONFIG.lang}>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="192x192"
