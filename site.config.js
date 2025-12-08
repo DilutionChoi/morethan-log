bcf196dc9935923c42e2303494d0df980a21d2c9
Heeseok Choi <tjr9320@gmail.com>
2025-12-08 09:56:30 -0500

    Fix link format in site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://odyssea-nova.vercel.app/",
+  link: "https://odyssea-nova.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
