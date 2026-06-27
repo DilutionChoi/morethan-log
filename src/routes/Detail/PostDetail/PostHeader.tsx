e906ccfc93ae835eda094d29c36dda35dfcd809c
DilutionChoi <tjr9320@gmail.com>
2026-06-27 17:15:20 +0900

    fix: fallback to profile name when Notion API omits author name

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -17,17 +17,17 @@ const PostHeader: React.FC<Props> = ({ data }) => {
       {data.type[0] !== "Paper" && (
         <nav>
           <div className="top">
-            {data.author && data.author[0] && data.author[0].name && (
+            {(data.author?.[0]?.name || CONFIG.profile.name) && (
               <>
                 <div className="author">
                   <Image
                     css={{ borderRadius: "50%" }}
-                    src={data.author[0].profile_photo || CONFIG.profile.image}
+                    src={data.author?.[0]?.profile_photo || CONFIG.profile.image}
                     alt="profile_photo"
                     width={24}
                     height={24}
                   />
-                  <div className="">{data.author[0].name}</div>
+                  <div className="">{data.author?.[0]?.name || CONFIG.profile.name}</div>
                 </div>
                 <div className="hr"></div>
               </>
